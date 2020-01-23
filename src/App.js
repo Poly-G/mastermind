1227cc04ab3e948ad37b7efe39bdea52dfaa64e6
Poly Goveia <poly.goveia@gmail.com>
2020-01-23 15:45:45 -0800

    create a get request with the specific parameters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = {
+    numbers: []
+  };
+  componentDidMount() {
+    axios
+      .get(
+        "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+      )
+      .then(res => {
+        this.setState({ numbers: res.data });
+      })
+      .catch(error => console.log(error));
+  }
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.numbers}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
