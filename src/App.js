a6de6b57e989d10d1489d27a4ea8341a53c843bc
Poly Goveia <poly.goveia@gmail.com>
2020-02-02 11:34:45 -0800

    make initial directions its own functional component

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 // import components
 import UserInput from "./components/userinput/UserInput";
 import Timer from "./components/timer/Timer";
+import InitialDirections from "./components/initialDirections/InitialDirections";
 
 class App extends React.Component {
   state = {
     randomNumber: [],
     modal: true
   };
-
-  toggle = () => this.setState({ modal: !this.state.modal });
 
   componentDidMount() {
     axios
@@ -27,50 +26,15 @@ class App extends React.Component {
       .catch(error => console.log(error));
   }
 
+  toggle = () => this.setState({ modal: !this.state.modal });
+
   render() {
     return (
       <div className="App">
+        <InitialDirections modal={this.state.modal} toggle={this.toggle} />
         {this.state.modal ? "" : <Timer className="timer" />}
-        <Modal isOpen={this.state.modal}>
-          <ModalHeader>
-            <h1>YOU'VE MADE IT THIS FAR</h1>
-          </ModalHeader>
-          <ModalBody>
-            <p>
-              You have successfully broken into the bank... now all you need to
-              do is break into this vault
-            </p>
-            <p className="intro-steps">Here are a few things we know:</p>
-            <ul className="steps">
-              <li>
-                You have <span className="direction-nums">10 </span>attempts and{" "}
-                <span className="direction-nums">2 minutes</span> before the
-                cops show up
-              </li>
-              <li>
-                This is a <span className="direction-nums">4</span> number
-                combination vault. If you try to input anything other than a
-                number is will warn you
-              </li>
-              <li>
-                This vault gives off feedback. If you get a number in the
-                correct location it will let you know. If you get a correct
-                number in the incorrect location, it will also let you know
-              </li>
-            </ul>
-            <p>
-              Good luck, and try to be quick! I will be waiting for you outside
-            </p>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={this.toggle} className="modal-button">
-              BEGIN
-            </Button>
-          </ModalFooter>
-        </Modal>
-
-        <p>{this.state.randomNumber}</p>
         <UserInput randomNumber={this.state.randomNumber} />
+        <p>{this.state.randomNumber}</p>
       </div>
     );
   }
---
+++ src/components/initialDirections/InitialDirections.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+export default function InitialDirections(props) {
+  return (
+    <div>
+      <Modal isOpen={props.modal}>
+        <ModalHeader>
+          <h1>YOU'VE MADE IT THIS FAR</h1>
+        </ModalHeader>
+        <ModalBody>
+          <p>
+            You have successfully broken into the bank... now all you need to do
+            is break into this vault
+          </p>
+          <p className="intro-steps">Here are a few things we know:</p>
+          <ul className="steps">
+            <li>
+              You have <span className="direction-nums">10 </span>attempts and{" "}
+              <span className="direction-nums">2 minutes</span> before the cops
+              show up
+            </li>
+            <li>
+              This is a <span className="direction-nums">4</span> number
+              combination vault. If you try to input anything other than a
+              number is will warn you
+            </li>
+            <li>
+              This vault gives off feedback. If you get a number in the correct
+              location it will let you know. If you get a correct number in the
+              incorrect location, it will also let you know
+            </li>
+          </ul>
+          <p>
+            Good luck, and try to be quick! I will be waiting for you outside
+          </p>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={props.toggle} className="modal-button">
+            BEGIN
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
