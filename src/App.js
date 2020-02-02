c9ec70a848d7ff280ecdd20c8d9c2b51cb506c37
Poly Goveia <poly.goveia@gmail.com>
2020-02-02 10:45:36 -0800

    change game background and color scheme

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
 // import components
 import UserInput from "./components/userinput/UserInput";
 
 class App extends React.Component {
   state = {
-    randomNumber: []
+    randomNumber: [],
+    modal: true
   };
+
+  toggle = () => this.setState({ modal: !this.state.modal });
 
   componentDidMount() {
     axios
@@ -24,6 +29,39 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Modal isOpen={this.state.modal}>
+          <ModalHeader>
+            <h1>YOU'VE MADE IT THIS FAR</h1>
+          </ModalHeader>
+          <ModalBody>
+            <p>
+              You have successfully broken into the bank... now all you need to
+              do is break into this vault
+            </p>
+            <p className="intro-steps">Here are a few things we know:</p>
+            <ul className="steps">
+              <li>You have 10 attempts before the cops show up</li>
+              <li>
+                This is a 4 number combination vault. If you try to input
+                anything other than a number is will warn you
+              </li>
+              <li>
+                This vault gives off feedback. If you get a number in the
+                correct location it will let you know. If you get a correct
+                number in the incorrect location, it will also let you know
+              </li>
+            </ul>
+            <p>
+              Good luck, and try to be quick! I will be waiting for you outside
+            </p>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={this.toggle} className="modal-button">
+              BEGIN
+            </Button>
+          </ModalFooter>
+        </Modal>
+
         <p>{this.state.randomNumber}</p>
         <UserInput randomNumber={this.state.randomNumber} />
       </div>
