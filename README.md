7c8345f30d404d30fd15b2aa329d081dd6030d30
Poly Goveia <poly.goveia@gmail.com>
2020-02-03 14:32:04 -0800

    update README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # MASTERMIND
+
+Welcome to MASTERMIND. May the odds be ever in your favor.
+
+## STORY LINE
+
+You and I have are in the process of robbing a bank. I am outside waiting in a white van giving you instructions and feedback on how to break into the vault.
+
+The vault is a 4 number combination vault, therefore it will only take numbers as an input. You have 2 minutes and 10 attempts in order to crack the code.
+
+## HOW TO PLAY
+
+There are 2 ways you can play:
+
+1.  Go to the live link on [http://mastermind-project.com/](http://mastermind-project.com/).
+
+2.  Follow these steps to play locally on your computer:
+
+    1. Clone this repo onto your local machine.
+    2. Navigate to the root folder of the repo and install the dependencies via `yarn install`.
+    3. Once you have successfully installed the dependencies, `yarn start` will start the game up.
+
+Once you have one of the steps above working then you can start playing!
+
+## Game Rules
+
+1. Once you click begin the timer will start. You have `2` minutes and `10` attempts to guess the correct number.
+2. The vault number will always be a combination of `4` numbers
+3. You can only input numbers.
+4. You will see your feedback in 2 places, Current Guess and History. Use History to help you figure out the number pattern.
+5. If you guess the number correctly within 2 minutes and 10 attempts you win, otherwise you lose.
--- src/components/initialDirections/InitialDirections.js
+++ src/components/initialDirections/InitialDirections.js
@@ -13,7 +13,7 @@ export default function InitialDirections(props) {
             You have successfully broken into the bank... now all you need to do
             is break into this vault
           </p>
-          <p className="intro-steps">Here are a few things we know:</p>
+          <p className="intro-steps">Here's what you'll need to know:</p>
           <ul className="steps">
             <li>
               You have <span className="direction-nums">10 </span>attempts and{" "}
@@ -23,16 +23,16 @@ export default function InitialDirections(props) {
             <li>
               This is a <span className="direction-nums">4</span> number
               combination vault. If you try to input anything other than a
-              number is will warn you
+              number it will warn you.
             </li>
             <li>
-              This vault gives off feedback. If you get a number in the correct
+              This vault provides feedback. If you get a number in the correct
               location it will let you know. If you get a correct number in the
-              incorrect location, it will also let you know
+              incorrect location, it will also let you know.
             </li>
           </ul>
           <p>
-            Good luck, and try to be quick! I will be waiting for you outside
+            Good luck, and try to be quick! I will be waiting for you outside.
           </p>
         </ModalBody>
         <ModalFooter>
